e0b58103f6fa20942c229fdd7f838a9963221a25
AgniAditya <agniaditya05@gmail.com>
2025-04-13 15:27:53 +0530

    first commit

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    """
+    Run the pipeline
+    """
+    train_pipeline(data_path="G:/My Stuff/2_ML/MLOps/data/olist_customers_dataset.csv")
